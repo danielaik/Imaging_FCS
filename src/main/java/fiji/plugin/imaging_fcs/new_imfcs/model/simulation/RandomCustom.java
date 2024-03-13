d0083165c89136786f3c13470fb19b64f5dec357
Marc Demoustier <marc.demoustier@epita.fr>
2024-03-13 15:16:20 +0800

    feat: add RandomCustom class to extend java random class with poisson random number generator

---
+++ src/main/java/fiji/plugin/imaging_fcs/new_imfcs/model/simulation/RandomCustom.java
@@ -0,0 +1,26 @@
+package fiji.plugin.imaging_fcs.new_imfcs.model.simulation;
+
+import java.util.Random;
+
+public class RandomCustom extends Random {
+
+    public RandomCustom() {
+        super();
+    }
+
+    public RandomCustom(int seed) {
+        super(seed);
+    }
+
+    public int nextPoisson(double mean) {
+        double L = Math.exp(-mean);
+        int k = 0;
+        double p = 1.0;
+        do {
+            k++;
+            double u = nextDouble(); // Generate a uniform random number u in [0,1]
+            p *= u;
+        } while (p > L);
+        return k - 1;
+    }
+}
