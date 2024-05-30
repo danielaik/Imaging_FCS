d2488a40608343ccd2ecd89b99cdfc31b026864c
Marc Demoustier <marc.demoustier@epita.fr>
2024-05-30 17:55:07 +0800

    feat: add an action listener to reset the parameters in the fit panel

--- src/main/java/fiji/plugin/imaging_fcs/new_imfcs/controller/FitController.java
+++ src/main/java/fiji/plugin/imaging_fcs/new_imfcs/controller/FitController.java
@@ -3,16 +3,26 @@ package fiji.plugin.imaging_fcs.new_imfcs.controller;
 import fiji.plugin.imaging_fcs.new_imfcs.model.FitModel;
 import fiji.plugin.imaging_fcs.new_imfcs.view.FitView;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class FitController {
     private final FitModel model;
     private final FitView view;
 
     public FitController(FitModel model) {
         this.model = model;
-        this.view = new FitView(model);
+        this.view = new FitView(this, model);
     }
 
     public void setVisible(boolean b) {
         view.setVisible(b);
+    }
+
+    public ActionListener btnResetParametersPressed() {
+        return (ActionEvent ev) -> {
+            model.setDefaultValues();
+            view.setDefaultValues();
+        };
     }
 }
