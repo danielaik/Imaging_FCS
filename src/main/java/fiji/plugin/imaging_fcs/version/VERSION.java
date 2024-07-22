6b93056ca2dcc1bff58800f3ce4d2c0853f5eed3
Daniel Aik <48349272+danielaik@users.noreply.github.com>
2024-07-22 05:15:55 +0200

    refactor(da version): increment version for testing using version 1_631 as template

--- src/main/java/fiji/plugin/imaging_fcs/version/VERSION.java
+++ src/main/java/fiji/plugin/imaging_fcs/version/VERSION.java
@@ -2,11 +2,11 @@ package fiji.plugin.imaging_fcs.version;
 
 public class VERSION {
 
-    public static final String IMFCS_VERSION = "v.1_63_1";               // increment for any changes in post-processing software
-    public static final String GPUFIT_VERSION = "v1_1_2";           // increment for any changes in gpufit
-    public static final String DCR_VERSION = "v1.29";            // increment for any changes in live readout feature
-    public static final String SDK2_VERSION = "v1_1_3";         // increment for any changes in c++ codebase
-    public static final String SDK3_VERSION = "v1_1_3";          // increment for any changes in c++ codebase
-    public static final String HAMASDK_VERSION = "v1_1_6";           // increment for any changes in c++ codebase
-    public static final String PVCAMSDK4_VERSION = "v1_1_5";             // increment for any changes in c++ codebase
+    public static final String IMFCS_VERSION = "v.1_632_da"; // increment for any changes in post-processing software
+    public static final String GPUFIT_VERSION = "v1_1_3"; // increment for any changes in gpufit
+    public static final String DCR_VERSION = "v1.30"; // increment for any changes in live readout feature
+    public static final String SDK2_VERSION = "v1_1_4"; // increment for any changes in c++ codebase
+    public static final String SDK3_VERSION = "v1_1_4"; // increment for any changes in c++ codebase
+    public static final String HAMASDK_VERSION = "v1_1_7"; // increment for any changes in c++ codebase
+    public static final String PVCAMSDK4_VERSION = "v1_1_6"; // increment for any changes in c++ codebase
 }
