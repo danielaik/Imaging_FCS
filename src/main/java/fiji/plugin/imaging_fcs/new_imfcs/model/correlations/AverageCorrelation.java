5bf5382202e69b3c40dc8ac5f6ef080c2c4e9ec3
Marc Demoustier <marc.demoustier@epita.fr>
2024-09-13 13:26:21 +0800

    feat: compute also the average of acf1 and acf2 if FCCS disp is used when computing the average correlation functions

--- src/main/java/fiji/plugin/imaging_fcs/new_imfcs/model/correlations/AverageCorrelation.java
+++ src/main/java/fiji/plugin/imaging_fcs/new_imfcs/model/correlations/AverageCorrelation.java
@@ -18,7 +18,7 @@ public final class AverageCorrelation {
     }
 
     /**
-     * Calculates the average autocorrelation function (ACF) and its variance for the specified ROI.
+     * Calculates the average correlation function (CF) and its variance for the specified ROI.
      * If the ROI is null, the calculation is performed on all valid PixelModel objects in the provided array.
      *
      * @param pixelModels           The 2D array of PixelModel objects.
@@ -46,36 +46,86 @@ public final class AverageCorrelation {
 
         int channelNumber = pixelModelList.get(0).getCorrelationFunction().length;
 
-        double[] averageAcf = new double[channelNumber];
-        double[] varianceAcf = new double[channelNumber];
+        double[] averageCF = new double[channelNumber];
+        double[] varianceCF = new double[channelNumber];
+        // define average array for acf1 and acf2 if FCCSDisp was used
+        double[][] averageDCCF = new double[2][channelNumber];
+        double[][] varianceDCCF = new double[2][channelNumber];
+
+        int numDCCFPixelsModels = 0;
 
         for (PixelModel currentPixelModel : pixelModelList) {
-            double[] acf = currentPixelModel.getCorrelationFunction();
-            for (int i = 0; i < channelNumber; i++) {
-                averageAcf[i] += acf[i];
-                varianceAcf[i] += acf[i] * acf[i];
+            accumulateCorrelationData(currentPixelModel.getCorrelationFunction(), averageCF, varianceCF);
+
+            if (currentPixelModel.getAcf1PixelModel() != null) {
+                numDCCFPixelsModels++;
+                double[] acf1 = currentPixelModel.getAcf1PixelModel().getCorrelationFunction();
+                double[] acf2 = currentPixelModel.getAcf2PixelModel().getCorrelationFunction();
+
+                accumulateCorrelationData(acf1, averageDCCF[0], varianceDCCF[0]);
+                accumulateCorrelationData(acf2, averageDCCF[1], varianceDCCF[1]);
             }
         }
 
-        for (int i = 0; i < channelNumber; i++) {
-            averageAcf[i] /= numPixelModels;
-            varianceAcf[i] = varianceAcf[i] / numPixelModels - Math.pow(averageAcf[i], 2.0);
+        computeAverages(averageCF, varianceCF, numPixelModels);
+        PixelModel averagePixelModel = createPixelModel(averageCF, varianceCF);
+
+        // Only compute and set DCCF data if DCCF models are present
+        if (numDCCFPixelsModels > 0) {
+            computeAverages(averageDCCF[0], varianceDCCF[0], numDCCFPixelsModels);
+            computeAverages(averageDCCF[1], varianceDCCF[1], numDCCFPixelsModels);
+
+            averagePixelModel.setAcf1PixelModel(createPixelModel(averageDCCF[0], varianceDCCF[0]));
+            averagePixelModel.setAcf2PixelModel(createPixelModel(averageDCCF[1], varianceDCCF[1]));
         }
 
-        return createPixelModel(averageAcf, varianceAcf);
+        return averagePixelModel;
     }
 
     /**
-     * Creates a PixelModel object with the given average ACF and variance ACF.
+     * Accumulates the correlation function (CF) values for averaging and variance calculation.
+     * This method adds the values from the correlation function (CF) to the corresponding average and variance arrays.
      *
-     * @param averageAcf  The array representing the average autocorrelation function.
-     * @param varianceAcf The array representing the variance of the autocorrelation function.
+     * @param cf         The correlation function values for the current pixel model.
+     * @param averageCF  The array storing the cumulative sum of correlation function values for averaging.
+     * @param varianceCF The array storing the cumulative sum of squared correlation function values for variance
+     *                   calculation.
+     */
+    private static void accumulateCorrelationData(double[] cf, double[] averageCF, double[] varianceCF) {
+        for (int i = 0; i < cf.length; i++) {
+            averageCF[i] += cf[i];
+            varianceCF[i] += cf[i] * cf[i];
+        }
+    }
+
+    /**
+     * Computes the average and variance of the correlation function (CF) values.
+     * This method calculates the average CF and variance for each channel based on the number of pixel models.
+     *
+     * @param averageCF      The array storing the cumulative sum of CF values for averaging, updated with the final
+     *                       averages.
+     * @param varianceCF     The array storing the cumulative sum of squared CF values, updated with the final
+     *                       variances.
+     * @param numPixelModels The number of pixel models used in the calculation.
+     */
+    private static void computeAverages(double[] averageCF, double[] varianceCF, int numPixelModels) {
+        for (int i = 0; i < averageCF.length; i++) {
+            averageCF[i] /= numPixelModels;
+            varianceCF[i] = varianceCF[i] / numPixelModels - Math.pow(averageCF[i], 2.0);
+        }
+    }
+
+    /**
+     * Creates a PixelModel object with the given average CF and variance CF.
+     *
+     * @param averageCF  The array representing the average correlation function.
+     * @param varianceCF The array representing the variance of the correlation function.
      * @return A PixelModel object containing the average ACF and its variance.
      */
-    private static PixelModel createPixelModel(double[] averageAcf, double[] varianceAcf) {
+    private static PixelModel createPixelModel(double[] averageCF, double[] varianceCF) {
         PixelModel averagePixelModel = new PixelModel();
-        averagePixelModel.setCorrelationFunction(averageAcf);
-        averagePixelModel.setVarianceCF(varianceAcf);
+        averagePixelModel.setCorrelationFunction(averageCF);
+        averagePixelModel.setVarianceCF(varianceCF);
 
         return averagePixelModel;
     }
