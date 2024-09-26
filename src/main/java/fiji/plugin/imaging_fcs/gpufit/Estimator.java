f1ffde4f5d7653d088818eb8f6a3c0488a9f4c29
Marc Demoustier <marc.demoustier@epita.fr>
2024-09-26 09:48:55 +0800

    refacto: separate gpufit java into multiple files

---
+++ src/main/java/fiji/plugin/imaging_fcs/gpufit/Estimator.java
@@ -0,0 +1,27 @@
+package fiji.plugin.imaging_fcs.gpufit;
+
+/**
+ * Java binding for Gpufit, a Levenberg Marquardt curve fitting library written in CUDA
+ * See https://github.com/gpufit/Gpufit, http://gpufit.readthedocs.io/en/latest/bindings.html#java
+ * <p>
+ * The available estimators. See the documentation for details.
+ */
+public enum Estimator {
+
+    /**
+     * Least-squares estimator
+     */
+    LSE(0),
+
+    /**
+     * Poisson maximum likelihood estimator
+     */
+    MLE(1);
+
+    public final int id;
+
+    Estimator(int id) {
+        this.id = id;
+    }
+
+}
