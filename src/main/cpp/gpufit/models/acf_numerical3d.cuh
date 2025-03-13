44ff598f64188e26c6d6a06b915e9a73aa53bb7b
Marc Demoustier <marc.demoustier@epita.fr>
2025-03-13 17:30:03 +0800

    fix: fix volume3d computation in acf3d computation on gpu

--- src/main/cpp/gpufit/models/acf_numerical3d.cuh
+++ src/main/cpp/gpufit/models/acf_numerical3d.cuh
@@ -8,21 +8,23 @@
 // --- Helper Math Function Macros ---
 #ifdef GPUFIT_DOUBLE
 #    define my_exp(x) exp(x)
+#    define my_pow(x, y) pow(x, y)
+#    define my_sqrt(x) sqrt(x)
 #    define my_erf(x) erf(x)
 #    define my_fabs(x) fabs(x)
 #else
 #    define my_exp(x) __expf(x)
+#    define my_pow(x, y) __powf(x, y)
 #    define my_erf(x) erff(x)
 #    define my_fabs(x) fabsf(x)
+#    define my_sqrt(x) sqrtf(x)
 #endif
 
-// --- Constants ---
+// Constants
 #define Z_STEPS 80
 #define TOTAL_ITERATIONS (Z_STEPS * Z_STEPS)
 #define CENTER_Z 40
 #define DIVISOR_Z 20.0
-
-// --- Helper Inline Functions ---
 
 __device__ inline void computeComponent(REAL x, REAL a, REAL r, REAL sdt, REAL sp0t, REAL p0t, REAL SQRT_PI, REAL *part,
                                         REAL *deriv)
@@ -65,7 +67,7 @@ __device__ void calculate_acfnumerical3d(REAL const *parameters, int const n_fit
 
     // --- Constants and Parameters ---
     const REAL PI = CUDART_PI;
-    const REAL SQRT_PI = sqrt(PI);
+    const REAL SQRT_PI = my_sqrt(PI);
     const REAL ridx = 1.3333;
 
     REAL N = parameters[0];
@@ -81,15 +83,27 @@ __device__ void calculate_acfnumerical3d(REAL const *parameters, int const n_fit
     REAL emLambda = parameters[20];
     REAL NA = parameters[21];
 
-    // --- Precomputed Derived Values ---
-    REAL srn = sqrt(ridx * ridx - NA * NA);
+    // Precomputed Derived Values
+    REAL srn = my_sqrt(ridx * ridx - NA * NA);
     REAL psfz = 2 * (emLambda / 1e9) * ridx / (NA * NA);
-    REAL szeff = sqrt(1.0 / (pow(sz, -2.0) + pow(psfz, -2.0)));
-    REAL sdt = sqrt(D * x);
+    REAL szeff = my_sqrt(1.0 / (my_pow(sz, -2.0) + my_pow(psfz, -2.0)));
+    REAL sdt = my_sqrt(D * x);
     REAL dt1 = -0.5 * x / (sdt * sdt * sdt);
     REAL dz = (sz * sz) / 400.0;
 
-    // --- Precompute z and psfz Arrays ---
+    // Compute volume 3D
+    REAL p00 = s;
+    REAL p1x0 = ax;
+    REAL pexpx0 = 2 * my_exp(- (p1x0 / p00) * (p1x0 / p00)) - 2;
+    REAL perfx0 = 2 * p1x0 * my_erf(p1x0 / p00);
+    REAL term_x = (p00 / SQRT_PI) * pexpx0 + perfx0;
+    REAL p1y0 = ay;
+    REAL pexpy0 = 2 * my_exp(- (p1y0 / p00) * (p1y0 / p00)) - 2;
+    REAL perfy0 = 2 * p1y0 * my_erf(p1y0 / p00);
+    REAL term_y = (p00 / SQRT_PI) * pexpy0 + perfy0;
+    REAL volume3d = SQRT_PI * szeff * 4 * my_pow(ax * ay, 2) / (term_x * term_y);
+
+    // Precompute z and psfz Arrays
     REAL z_values[Z_STEPS];
     REAL psfz_values[Z_STEPS];
 
@@ -117,7 +131,7 @@ __device__ void calculate_acfnumerical3d(REAL const *parameters, int const n_fit
         REAL psfz2 = psfz_values[z2_index];
 
         REAL p0t = (8 * D * x + psfz1 * psfz1 + psfz2 * psfz2) / 2.0;
-        REAL sp0t = sqrt(p0t);
+        REAL sp0t = my_sqrt(p0t);
 
         REAL xPart, xDeriv, yPart, yDeriv;
         computeComponent(x, ax, rx, sdt, sp0t, p0t, SQRT_PI, &xPart, &xDeriv);
@@ -133,15 +147,14 @@ __device__ void calculate_acfnumerical3d(REAL const *parameters, int const n_fit
         sumd1 += zExp * ((dt1 + dt2) * xPart * yPart + xDeriv * yPart + xPart * yDeriv) * dz;
     }
 
-    // --- Final Calculations ---
-    REAL volume3d = SQRT_PI * szeff * (4 * pow(ax * ay, 2));
-    REAL acf1 = (sum1 * 1e6) / (4 * pow(ax * ay, 2) / (volume3d / (SQRT_PI * sz)));
-    REAL Dpspim = (sumd1 * 1e6) / (4 * pow(ax * ay, 2) / (volume3d / (SQRT_PI * sz)));
+    // Final Calculations
+    REAL acf1 = (sum1 * 1e6) / (4 * my_pow(ax * ay, 2) / (volume3d / (SQRT_PI * sz)));
+    REAL Dpspim = (sumd1 * 1e6) / (4 * my_pow(ax * ay, 2) / (volume3d / (SQRT_PI * sz)));
     REAL triplet = 1.0 + fTrip / (1.0 - fTrip) * my_exp(-x / tTrip);
 
     value[point_index] = (1.0 / N)
-            * (((1 - parameters[5]) * acf1 + pow(parameters[18], 2) * parameters[5] * 0.0)
-               / (pow(1 - parameters[5] + parameters[18] * parameters[5], 2)))
+            * (((1 - parameters[5]) * acf1 + my_pow(parameters[18], 2) * parameters[5] * 0.0)
+               / (my_pow(1 - parameters[5] + parameters[18] * parameters[5], 2)))
             * triplet
         + parameters[4];
 
