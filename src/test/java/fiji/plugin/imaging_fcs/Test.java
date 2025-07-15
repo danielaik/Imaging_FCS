2029ce5481c65915ba96ed8982316bff320ff6ae
Daniel Aik <48349272+danielaik@users.noreply.github.com>
2025-07-15 14:49:03 +0200

    enable local testing with mvn test

--- src/test/java/fiji/plugin/imaging_fcs/Test.java
+++ src/test/java/fiji/plugin/imaging_fcs/Test.java
@@ -1,17 +1,37 @@
+
 package fiji.plugin.imaging_fcs;
 
 import ij.ImageJ;
 import fiji.plugin.imaging_fcs.imfcs.Imaging_FCS;
-import java.io.File;
+import static org.junit.Assert.assertTrue;
 
 public class Test {
 
-    public static void main(String[] args) {
-//        System.getProperties().setProperty("plugins.dir", System.getProperty("user.dir") + File.separator + "dist" + File.separator);
-//        ImageJ ij = new ImageJ();
-//        imfcs.Imaging_FCS imfcsObj = new Imaging_FCS();
-//        imfcsObj.run("start");
-//        System.out.println("getInfo: " + ij.getInfo());
+    @org.junit.Test
+    public void testApp() {
+
+        // Simple test that always passes
+        assertTrue(true);
+
+        // Initialize ImageJ
+        ImageJ ij = new ImageJ();
+
+        // Create an instance of imaging FCS
+        Imaging_FCS pluginImFCS = new Imaging_FCS();
+
+        // Call the run method with an argument (can be empty if not used)
+        pluginImFCS.run("start");
+
+        // Prevent ImageJ from closing immediately by adding sleep
+        try {
+            Thread.sleep(1000000); // Keep the UI open for 10 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace(); // Handle the interruption
+        }
+
+        // Print ImageJ information
+        System.out.println("getInfo: " + ij.getInfo());
+
     }
 
 }
