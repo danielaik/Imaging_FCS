cbfe8b481c783770c7f3d55ea7d24ca8de7ae26f
Daniel Aik <danielaikyk@gmail.com>
2025-10-06 14:39:39 +0200

    enable mvn test

--- src/test/java/fiji/plugin/imaging_fcs/Test.java
+++ src/test/java/fiji/plugin/imaging_fcs/Test.java
@@ -1,17 +1,32 @@
 package fiji.plugin.imaging_fcs;
 
 import ij.ImageJ;
-import fiji.plugin.imaging_fcs.imfcs.Imaging_FCS;
-import java.io.File;
+import fiji.plugin.imaging_fcs.imfcs.ImagingFCS;
+import static org.junit.Assert.assertTrue;
 
 public class Test {
 
-    public static void main(String[] args) {
-//        System.getProperties().setProperty("plugins.dir", System.getProperty("user.dir") + File.separator + "dist" + File.separator);
-//        ImageJ ij = new ImageJ();
-//        imfcs.Imaging_FCS imfcsObj = new Imaging_FCS();
-//        imfcsObj.run("start");
-//        System.out.println("getInfo: " + ij.getInfo());
+    @org.junit.Test
+    public void testApp() {
+
+        // Simple test that always passes
+        assertTrue(true);
+
+        ImageJ ij = new ImageJ();
+
+        ImagingFCS pluginImFCS = new ImagingFCS();
+
+        pluginImFCS.run("start");
+
+        // Prevent ImageJ from closing immediately by adding sleep
+        try {
+            Thread.sleep(1000000); // Keep the UI open for 10 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace(); // Handle the interruption
+        }
+
+        System.out.println("getInfo: " + ij.getInfo());
+
     }
 
 }
